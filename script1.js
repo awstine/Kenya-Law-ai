d1cd4fbf73cb6484c6737297489223fbe527af02
Остин Джесси <116760268+awstine@users.noreply.github.com>
2025-03-13 18:03:29 -0700

    updated the login page

--- script1.js
+++ script1.js
@@ -7,3 +7,12 @@ logoutBtn.addEventListener('click', function() {
     // Redirect to the login screen
     window.location.href = 'login.html';
 });
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Perform any validation or other logic here
+
+    // Redirect to the login page
+    window.location.href = 'login.html';
+  });
